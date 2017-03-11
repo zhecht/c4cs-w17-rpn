5e2f49674dd96d1673df0fd56ed32d3d0a7773ab
Zack Hecht <hechtor@Zacks-MacBook-Pro.local>
2017-03-11 18:19:25 -0500

    added expo

--- rpn.py
+++ rpn.py
@@ -5,6 +5,7 @@ operators = {
 	'-': operator.sub,
 	'*': operator.mul,
 	'/': operator.truediv,
+	'^': operator.pow,
 }
 def calculate(myarg):
 	stack = list()
--- test_rpn.py
+++ test_rpn.py
@@ -13,6 +13,9 @@ class TestBasics(unittest.TestCase):
 	def test_divide(self):
 		result = rpn.calculate("6 3 /")
 		self.assertEqual(2, result)
+	def test_expo(self):
+		result = rpn.calculate("2 3 ^")
+		self.assertEqual(8,result)
 	def test_badstring(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
